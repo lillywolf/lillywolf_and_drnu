01936efed6198cde0dbabea43d37854d537fdfaf
lillywolf <lillywolf@gmail.com>
2011-09-29 00:57:50 -0700

    updated routs

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "site", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
@@ -42,5 +42,4 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id.:format'
   
   map.resources :site  
-  
 end
